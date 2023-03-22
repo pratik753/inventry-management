7009b297213f569835580eb81a9fc9b3ad93dc2e
deabshish <debashishmahato555@gmail.com>
2023-03-22 23:39:29 +0530

    lak

--- client/src/components/Home/TodaySale.js
+++ client/src/components/Home/TodaySale.js
@@ -1,13 +1,75 @@
-import React from 'react'
-
-const TodaySale = () => {
-
-
-
-
-    return (
-        <div>TodaySale</div>
-    )
+import React, { useEffect, useState } from "react";
+import { styled } from "@mui/material/styles";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell, { tableCellClasses } from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+import { getInventory } from "../../api";
+import Axios from "axios";
+const StyledTableCell = styled(TableCell)(({ theme }) => ({
+  [`&.${tableCellClasses.head}`]: {
+    backgroundColor: theme.palette.common.black,
+    color: theme.palette.common.white,
+  },
+  [`&.${tableCellClasses.body}`]: {
+    fontSize: 14,
+  },
+}));
+function createData(name, calories, fat, carbs, protein) {
+  return { name, calories, fat, carbs, protein };
 }
 
-export default TodaySale
+const rows = [
+  createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
+  createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
+  createData("Eclair", 262, 16.0, 24, 6.0),
+  createData("Cupcake", 305, 3.7, 67, 4.3),
+  createData("Gingerbread", 356, 16.0, 49, 3.9),
+];
+
+const TodaySale = () => {
+  const [udata, setUData] = useState([]);
+  const getData = async () => {
+    const data = await Axios.get("http://localhost:5000/sale/getSale");
+    setUData(data.data);
+    //  console.log(data.data)
+  };
+  useEffect(() => {
+    getData();
+  }, []);
+
+  return (
+    <TableContainer component={Paper} style={{ margin: "4rem" }}>
+      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+        <TableHead style={{ backgroundClip: "blue" }}>
+          <TableRow>
+            <StyledTableCell>ProductName </StyledTableCell>
+            <StyledTableCell align="right">Price</StyledTableCell>
+            <StyledTableCell align="right">Quantity</StyledTableCell>
+            <StyledTableCell align="right">purchaseDate</StyledTableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {udata.map((row) => (
+            <TableRow
+              key={row._id}
+              sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+            >
+              <TableCell component="th" scope="row">
+                {row.productName}
+              </TableCell>
+              <TableCell align="right">{row.price}</TableCell>
+              <TableCell align="right">{row.quantity}</TableCell>
+              <TableCell align="right">{row.purchaseDate}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+};
+
+export default TodaySale;
